57e748b15554480c9a060e79419ecf3cc21ba4e4
Ruixin Lee <41141489+Breeze1in1drizzle@users.noreply.github.com>
2021-03-29 11:23:26 +0800

    Offline analysis

---
+++ MindLink-HeartCare/offline_analysis/OfflineAnalysis.py
@@ -0,0 +1,13 @@
+import EVM_V1 as evm
+import heartRateObserver as hro
+
+
+def main():
+    # 首先读取视频文件(*.mp4)
+    # 然后进行欧拉放大，保存成为放大后的文件
+    # 再读取新的文件，计算每一分钟图片的心率
+    pass
+
+
+if __name__ == "__main__":
+    pass
