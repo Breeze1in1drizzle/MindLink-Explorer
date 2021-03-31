9b39ff154264f3e89e6d33c1103a5c5e721a67ad
Ruixin Lee <41141489+Breeze1in1drizzle@users.noreply.github.com>
2021-03-31 09:43:40 +0800

    updates offline analysis

--- MindLink-HeartCare/offline_analysis/OfflineAnalysis.py
+++ MindLink-HeartCare/offline_analysis/OfflineAnalysis.py
@@ -1,13 +1,24 @@
-import EVM_V1 as evm
-import heartRateObserver as hro
+# -*- coding: utf-8 -*-
+'''
+欧拉放大视频，然后计算视频钟的心率
+'''
+import EVM_V1 as EVM
+import heartRateObserver as HRO
 
 
-def main():
+def offline_analysis_for_one_video(vidoe_path='', save_path=''):
     # 首先读取视频文件(*.mp4)
-    # 然后进行欧拉放大，保存成为放大后的文件
+    evm = EVM.EVM_tools()
+    # 然后进行欧拉放大，保存放大后的视频文件
+    evm.magnify_color("baby.mp4", 0.4, 3)
+    ###########################################
+    #####上述欧拉放大步骤需要调整一下保存路径########
+    ###########################################
     # 再读取新的文件，计算每一分钟图片的心率
-    pass
+    hro = HRO.heartRateObserver()       # 这个类没有文件的IO和处理功能
+    hro.run()       # 这里需要把在线分析代码改为离线分析代码，同时还要增加保存数据的函数
 
 
 if __name__ == "__main__":
-    pass
+    video_path = 'baby.mp4'
+    offline_analysis_for_one_video(vidoe_path=video_path)
